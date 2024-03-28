fab332adc619c82fea90c62250019eff2f60eb7c
emeryisgoat23 <165311016+emeryisgoat23@users.noreply.github.com>
2024-03-28 13:09:52 -0400

    Update chestESP.js

--- gold/chestESP.js
+++ gold/chestESP.js
@@ -1,4 +1,5 @@
-(async () => {
+(async () => {Javascript
+              
     let n = document.createElement('iframe');
     document.body.append(n);
     window.alert = n.contentWindow.alert.bind(window);
